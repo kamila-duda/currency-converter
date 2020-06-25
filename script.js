ea7c1f86caffbc69072b0732da5f1f6731589856
Kamila Duda <kakus86@o2.pl>
2020-06-25 16:36:54 +0200

    changed let on const

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 {
-    const choseCurency = (selected) => {
-        let currency = 1;
-        if(selected!=="pln") {
-             currency = Number(document.querySelector(".js-"+selected).innerHTML);
+    const chooseCurrency = (selected) => {
+        if(selected==="pln") {
+            return 1; 
         }
-        return currency;
+        return Number(document.querySelector(`.js-${selected}`).innerHTML);
     }
     const calculate = () => {
         const selectFirst = document.querySelector(".js-form__selectFirst");
         const selectSecond = document.querySelector(".js-form__selectSecond");
         const amount = Number(document.querySelector(".js-amount").value);
-        let result = "";
-        let symbol = "PLN";
-        const currency1 = choseCurency(selectFirst.value);
-        const currency2 = choseCurency(selectSecond.value);
-        result = amount*currency1/currency2;
-        symbol = selectSecond.value;
+        const currency1 = chooseCurrency(selectFirst.value);
+        const currency2 = chooseCurrency(selectSecond.value);
+        const result = amount*currency1/currency2;
+        const symbol = selectSecond.value;
         document.querySelector(".js-currencyConverter__result").innerHTML = `${result.toFixed(2)} ${symbol}`;
 
     };
